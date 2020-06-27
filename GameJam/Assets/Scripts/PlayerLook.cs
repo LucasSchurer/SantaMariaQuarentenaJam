bc8f9f54ceff1014987e237b88f5802d1ed1d104
Lucas Schürer <39245000+LucasSchurer@users.noreply.github.com>
2020-06-26 23:31:20 -0300

    Added basic player movement

---
+++ GameJam/Assets/Scripts/PlayerLook.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+    private Transform cameraTransform;
+    public Transform playerModelTransform;
+    
+    public float sensitivity = 100f;
+    private float cameraRotation;
+
+    void Awake()
+    {
+        cameraTransform = Camera.main.transform;
+    }
+
+    void Update()
+    {
+        float inputX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        float inputY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+
+        cameraRotation -= inputY;
+        cameraRotation = Mathf.Clamp(cameraRotation, -90f, 90f);
+
+        cameraTransform.localRotation = Quaternion.Euler(cameraRotation, 0f, 0f);
+        playerModelTransform.Rotate(Vector3.up * inputX);
+    }
+}
---
+++ GameJam/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController controller;
+    public float movementSpeed = 10f;
+    private Vector3 velocity;
+    public float gravity = 9f;
+    public float jumpVelocity = 4f;
+    public bool isGrounded;
+    
+    void Awake()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = controller.isGrounded;
+
+        float inputX = Input.GetAxis("Horizontal");
+        float inputZ = Input.GetAxis("Vertical");
+
+        Vector3 movement = controller.transform.right * inputX + controller.transform.forward * inputZ;
+        movement *= movementSpeed;
+
+        if (controller.isGrounded)
+            velocity.y = 0f;
+
+        velocity.y -= gravity * Time.deltaTime;
+
+        movement.y = velocity.y;
+
+        controller.Move(movement * Time.deltaTime);
+    }
+}
