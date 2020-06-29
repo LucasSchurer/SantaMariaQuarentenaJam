46ddc0cb4fcff89fa9389bbfe5ad3a8f17dbb87f
Jonstiii <jonstiplay@gmail.com>
2020-06-29 17:37:26 -0300

    SoundsV1

---
+++ GameJam1/Assets/Scripts/SoundOnTrigger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundOnTrigger : MonoBehaviour
+{
+
+    public AudioSource TocarSom;
+
+    void OnTriggerEnter(Collider other)
+    {
+        TocarSom.Play();  
+    }
+
+}
