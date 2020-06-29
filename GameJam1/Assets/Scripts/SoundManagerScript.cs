acbc74d3276d18557aff4a3e43e7eaa07e8d4108
Jonstiii <jonstiplay@gmail.com>
2020-06-29 20:22:17 -0300

    SoundV1.3

---
+++ GameJam1/Assets/Scripts/SoundManagerScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManagerScript : MonoBehaviour
+{
+    public static AudioClip transitionSound, possessSound;
+    static AudioSource audioSrc;
+
+
+    void Start()
+    {
+        //transitionSound = Resources.Load<AudioClip>("");
+        //possessSound = Resources.Load<AudioClip>("");
+
+        audioSrc = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public static void PlaySound (string clip)
+    {
+        //switch (clip)
+        {
+           // case "transitionSound":
+               // audioSrc.PlayOneShot();
+               // break;
+           // case "possessSound":
+               // audioSrc.PlayOneShot();
+               // break;
+        }
+    }
+ }
+
