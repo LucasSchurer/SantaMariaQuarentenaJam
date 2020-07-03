6ef58653d2842bf179aa02c4e750a57558c79e7a
Lucas Schürer <39245000+LucasSchurer@users.noreply.github.com>
2020-07-03 05:40:47 -0300

    Update game

---
+++ GameJam1/Assets/Scripts/Interactable/Valve.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Valve : MonoBehaviour
+{
+    public Transform controlledPipe;
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    public void ChangePipeState()
+    {
+        if (controlledPipe != null)
+        {
+            if (controlledPipe.gameObject.activeInHierarchy)
+            {
+                StartCoroutine(ChangePipe());
+            } 
+            else
+            {
+                controlledPipe.gameObject.SetActive(true);
+                anim.SetTrigger("deactivated");
+            }
+        }
+    }
+
+    private IEnumerator ChangePipe()
+    {
+        anim.SetTrigger("activated");
+
+        yield return new WaitForSeconds(2f);
+        controlledPipe.gameObject.SetActive(false);
+
+        yield return null;
+    }
+}
